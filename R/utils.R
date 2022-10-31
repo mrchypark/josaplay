033e72fb488d92bbdd4049cf6c9f9a191b0b2de1
chanyub Park <mrchypark@gmail.com>
2022-10-31 10:45:56 +0900

    update 0.2.0

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,17 @@
 cvt_num2h <- function(number) {
-  . <- NULL
-  number %>%
-    as.character() %>%
-    switch(.,
-      "1" = 51068,
-      "2" = 51060,
-      "3" = 49340,
-      "4" = 49324,
-      "5" = 50724,
-      "6" = 50977,
-      "7" = 52832,
-      "8" = 54036,
-      "9" = 44396,
-      "0" = 49901
-    )
+  switch(
+    as.character(number),
+    "1" = 51068,
+    "2" = 51060,
+    "3" = 49340,
+    "4" = 49324,
+    "5" = 50724,
+    "6" = 50977,
+    "7" = 52832,
+    "8" = 54036,
+    "9" = 44396,
+    "0" = 49901
+  )
 }
 
 #' @importFrom utf8 as_utf8
@@ -26,21 +24,3 @@ utf8ToInt_as <- function(textko) {
 quite_as_numeric <- function(x, ...) {
   suppressWarnings(as.numeric(x, ...))
 }
-
-gwa <- intToUtf8(44284)
-wa <- intToUtf8(50752)
-
-eun <- intToUtf8(51008)
-neun <- intToUtf8(45716)
-
-i <- intToUtf8(51060)
-ga <- intToUtf8(44032)
-
-eul <- intToUtf8(51012)
-leul <- intToUtf8(47484)
-
-a <- intToUtf8(50500)
-ya <- intToUtf8(50556)
-
-ena <- intToUtf8(c(51060, 45208))
-na <- intToUtf8(45208)
